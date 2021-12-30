85cc6b2bfef6daf7237e927773cee2bb3c2c6f35
namnhi997 <namnhi.vnu@gmail.com>
2021-12-30 16:00:03 +0700

    first commit 5

--- js/ntechs.js
+++ js/ntechs.js
@@ -38,3 +38,26 @@ function erase() {
 document.addEventListener("DOMContentLoaded", function () { // On DOM Load initiate the effect
     if (textArray.length) setTimeout(type, newTextDelay + 250);
 });
+const getone = document.querySelector.bind(document);
+const getall = document.querySelectorAll.bind(document);
+const tabs = getall(".tab-item");
+const panes = getall(".products-list");
+const tabActive = getone(".tab-item.active");
+const line = getone(".line");
+line.style.left = tabActive.offsetLeft + "px";
+line.style.width = tabActive.offsetWidth + "px";
+tabs.forEach((tab, index) => {
+    const pane = panes[index];
+
+    tab.onclick = function () {
+        getone(".tab-item.active").classList.remove("active");
+        getone(".products-list.active").classList.remove("active");
+
+        line.style.left = this.offsetLeft + "px";
+        line.style.width = this.offsetWidth + "px";
+
+        this.classList.add("active");
+        pane.classList.add("active");
+    };
+});
+console.log(tabs, panes);
